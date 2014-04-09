b5823770c54e7f08371b490fdc7fbaaf8fe08d1b
Alex Petrov <alexp@coffeenco.de>
2014-04-09 14:03:15 +0200

    Re-organise and namespace metrics

--- src/clj/sigar_eventoverse/conversion.clj
+++ src/clj/sigar_eventoverse/conversion.clj
@@ -1,6 +1,6 @@
 (ns sigar-eventoverse.conversion
   (:import [java.lang.Character]
-           [org.hyperic.sigar CpuInfo CpuPerc ProcMem DiskUsage Tcp]))
+           [org.hyperic.sigar CpuInfo CpuPerc ProcMem ProcCpu DiskUsage FileSystemUsage Tcp Cpu Mem NetStat Swap ]))
 
 ;;
 ;; Implementation
@@ -9,11 +9,15 @@
 (defn camel-to-dash
   "Converts camel-cased string to dash-separated"
   [s]
-  (keyword (subs (apply str (map #(if (Character/isUpperCase %)
-                                    (str "-" (clojure.string/lower-case %))
-                                    %)
-                                 s))
-                 1)))
+  (subs (apply str (map #(if (Character/isUpperCase %)
+                           (str "-" (clojure.string/lower-case %))
+                           %)
+                        s))
+        1))
+
+(defn add-prefix
+  [prefix val]
+  (str prefix "." val))
 
 (defn- apply-to-keys [m f]
   "Applies function f to all values in map m"
@@ -25,15 +29,8 @@
   (into {} (for [[k v] m]
              [k (f v)])))
 
-(defn numerize
-  [v]
-  (if (string? v)
-    (read-string v)
-    v))
-
-(defn- numerize-values
-  [m]
-  (apply-to-values m numerize))
+(defn array-of [t]
+  (.getClass (java.lang.reflect.Array/newInstance t 0)))
 
 ;;
 ;; ToMap
@@ -45,43 +42,112 @@
 (extend-protocol ToMap
   CpuInfo
   (to-map [^CpuInfo cpu-info]
-    (numerize-values
-     (apply-to-keys
-      (.toMap cpu-info)
-      camel-to-dash)))
+    (apply-to-keys
+     (.toMap cpu-info)
+     camel-to-dash))
 
   CpuPerc
   (to-map [^CpuPerc cpu-perc]
-    (numerize-values
-     {:user (.getUser cpu-perc)
-      :sys (.getSys cpu-perc)
-      :idle (.getIdle cpu-perc)
-      :wait (.getWait cpu-perc)
-      :nice (.getNice cpu-perc)
-      :combined (.getCombined cpu-perc)
-      :irc (.getIrq cpu-perc)}))
+    {:system.cpu.user     (.getUser cpu-perc)
+     :system.cpu.sys      (.getSys cpu-perc)
+     :system.cpu.idle     (.getIdle cpu-perc)
+     :system.cpu.wait     (.getWait cpu-perc)
+     :system.cpu.nice     (.getNice cpu-perc)
+     :system.cpu.combined (.getCombined cpu-perc)
+     :system.cpu.irc      (.getIrq cpu-perc)})
+
+  Mem
+  (to-map [^Mem mem]
+    {:system.memory.actual_free  (.getActualFree mem)
+     :system.memory.actual_used  (.getActualUsed mem)
+     :system.memory.ram          (.getRam mem)
+     :system.memory.free         (.getFree mem)
+     :system.memory.total        (.getTotal mem)
+     :system.memory.used         (.getUsed mem)
+     :system.memory.used_percent (.getUsedPercent mem)
+     :system.memory.free_percent (.getFreePercent mem)})
+
+  NetStat
+  (to-map [^NetStat netstat]
+    {:system.netword.all_inbound_total  (.getAllInboundTotal netstat)
+     :system.netword.all_outbound_total (.getAllOutboundTotal netstat)
+     :system.netword.tcp_bound          (.getTcpBound netstat)
+     :system.network.tcp_close          (.getTcpClose netstat)
+     :system.network.tcp_close_wait     (.getTcpCloseWait netstat)
+     :system.network.tcp_closing        (.getTcpClosing netstat)
+     :system.network.tcp_established    (.getTcpEstablished netstat)
+     :system.network.tcp_fin_wait_1     (.getTcpFinWait1 netstat)
+     :system.network.tcp_fin_wait_2     (.getTcpFinWait2 netstat)
+     :system.network.tcp_idle           (.getTcpIdle netstat)
+     :system.network.tcp_inbound_total  (.getTcpInboundTotal netstat)
+     :system.network.tcp_last_ack       (.getTcpLastAck netstat)
+     :system.network.tcp_listen         (.getTcpListen netstat)
+     :system.network.tcp_outbound_total (.getTcpOutboundTotal netstat)
+     :system.network.tcp_syn_recv       (.getTcpSynRecv netstat)
+     :system.network.tcp_syn_sent       (.getTcpSynSent netstat)
+     :system.network.tcp_time_wait      (.getTcpTimeWait netstat)})
 
   ProcMem
   (to-map [^ProcMem proc-mem]
-    (numerize-values
-     {:size (.getSize proc-mem)
-      :resident (.getResident proc-mem)
-      :share (.getShare proc-mem)
-      :minor-faults (.getMinorFaults proc-mem)
-      :major-faults (.getMajorFaults proc-mem)})
-    )
+    {:system.process.memory.major-faults (.getMajorFaults proc-mem)
+     :system.process.memory.minor-faults (.getMinorFaults proc-mem)
+     :system.process.memory.page-faults  (.getPageFaults proc-mem)
+     :system.process.memory.resident     (.getResident proc-mem)
+     :system.process.memory.rss          (.getRss proc-mem)
+     :system.process.memory.share        (.getShare proc-mem)
+     :system.process.memory.size         (.getSize proc-mem)})
+
+
+  ProcCpu
+  (to-map [^ProcCpu proc-cpu]
+    {:system.process.cpu.last_time  (.getLastTime proc-cpu)
+     :system.process.cpu.percent    (.getPercent proc-cpu)
+     :system.process.cpu.start_time (.getStartTime proc-cpu)
+     :system.process.cpu.sys        (.getSys proc-cpu)
+     :system.process.cpu.total      (.getTotal proc-cpu)
+     :system.process.cpu.user       (.getUser proc-cpu)})
+
+  Swap
+  (to-map [^Swap swap]
+    {:system.swap.free     (.getFree swap)
+     :system.swap.page_in  (.getPageIn swap)
+     :system.swap.page_out (.getPageOut swap)
+     :system.swap.total    (.getTotal swap)
+     :system.swap.used     (.getUsed swap)})
 
   DiskUsage
   (to-map [^DiskUsage disk-usage]
-    (numerize-values
-     (apply-to-keys
-      (into {} (.toMap disk-usage))
-      camel-to-dash
-      )))
+    {:system.disk.usage.read_bytes   (.getReadBytes disk-usage)
+     :system.disk.usage.reads        (.getReads disk-usage)
+     :system.disk.usage.service_time (.getServiceTime disk-usage)
+     :system.disk.usage.write_bytes  (.getWriteBytes disk-usage)
+     :system.disk.usage.writes       (.getWrites disk-usage)})
 
   Tcp
   (to-map [^Tcp tcp]
-    (numerize-values
-     (apply-to-keys
-      (into {} (.toMap tcp))
-      camel-to-dash))))
+    {:system.network.tcp.active_opens  (.getActiveOpens tcp)
+     :system.network.tcp.attempt_fails (.getAttemptFails tcp)
+     :system.network.tcp.curr_estab    (.getCurrEstab tcp)
+     :system.network.tcp.estab_resets  (.getEstabResets tcp)
+     :system.network.tcp.in_errs       (.getInErrs tcp)
+     :system.network.tcp.in_segs       (.getInSegs tcp)
+     :system.network.tcp.out_rsts      (.getOutRsts tcp)
+     :system.network.tcp.out_segs      (.getOutSegs tcp)
+     :system.network.tcp.passive_opens (.getPassiveOpens tcp)
+     :system.network.tcp.retrans_segs  (.getRetransSegs tcp)})
+
+  FileSystemUsage
+  (to-map [^FileSystemUsage file-system]
+    {:system.fs.usage.available    (.getAvail file-system)
+     :system.fs.usage.queue        (.getDiskQueue file-system)
+     :system.fs.usage.read_bytes   (.getDiskReadBytes file-system)
+     :system.fs.usage.reads        (.getDiskReads file-system)
+     :system.fs.usage.service_time (.getDiskServiceTime file-system)
+     :system.fs.usage.write_bytes  (.getDiskWriteBytes file-system)
+     :system.fs.usage.writes       (.getDiskWrites file-system)
+     :system.fs.usage.files        (.getFiles file-system)
+     :system.fs.usage.free         (.getFree file-system)
+     :system.fs.usage.free_files   (.getFreeFiles file-system)
+     :system.fs.usage.total        (.getTotal file-system)
+     :system.fs.usage.used         (.getUsed file-system)
+     :system.fs.usage.used_percent (.getUsePercent file-system)}))
--- src/clj/sigar_eventoverse/core.clj
+++ src/clj/sigar_eventoverse/core.clj
@@ -3,32 +3,65 @@
            [org.hyperic.sigar Sigar SigarLoader])
   (:use [sigar-eventoverse.conversion]))
 
+(def instance (Sigar.))
+
 (defn get-cpu-list
   "List of cpus and their info"
   []
-  (map to-map (.getCpuInfoList (Sigar.))))
+  (map to-map (.getCpuInfoList instance)))
 
-(defn get-cpu-perc
+(defn get-cpu-perc-breakdown
   "Current cpu stats"
   []
-  (to-map (.getCpuPerc (Sigar.))))
+  (mapv to-map (.getCpuPercList instance)))
 
 (defn get-proc-mem
   "Process memory metrics"
   [pid]
-  (to-map (.getProcMem (Sigar.) pid)))
+  (to-map (.getProcMem instance pid)))
 
-(defn get-disk-usage
-  "Disk usage for certain disk"
-  [disk]
-  (to-map (.getDiskUsage (Sigar.) disk)))
+(defn get-proc-cpu
+  "Process memory metrics"
+  [pid]
+  (assoc (to-map (.getProcCpu instance pid))
+    :pid pid))
+
+(defn get-cpu-perc-combined
+  "Current cpu stats"
+  []
+  (to-map (.getCpuPerc instance)))
+
+(defn get-mem
+  "Current cpu stats"
+  []
+  (to-map (.getMem instance)))
+
+(defn get-netstat
+  "Current cpu stats"
+  []
+  (to-map (.getNetStat instance)))
+
+(defn get-netstat
+  "Current cpu stats"
+  []
+  (to-map (.getNetStat instance)))
 
 (defn get-tcp
   "Tcp stats"
   []
-  (to-map (.getTcp (Sigar.))))
+  (to-map (.getTcp instance)))
 
-(defn map-diff [m1 m2]
-  "Returns numeric difference between values of two maps. Useful for diff reporting."
-  (into {} (for [[k v] m2]
-             [k (- v (get m1 k))])))
+(defn get-swap
+  "Tcp stats"
+  []
+  (to-map (.getSwap instance)))
+
+(defn get-disk-usage
+  "Disk usage for certain disk"
+  [^String disk]
+  (to-map (.getDiskUsage instance disk)))
+
+(defn get-fs-usage
+  "Disk usage for certain disk"
+  [^String fs]
+  (to-map (.getFileSystemUsage instance fs)))
